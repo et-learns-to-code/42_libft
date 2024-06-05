fc188882d18054aac58cae0d871768b65e6420a1
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2024-06-05 16:31:25 +0800

    progress yee

---
+++ ft_toupper.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 15:55:43 by etien             #+#    #+#             */
+/*   Updated: 2024/06/05 16:01:39 by etien            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+/*
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char test_chars[] = {'a', 'z', 'A', 'Z', '0', '9', '!', ' '};
+    size_t num_tests = sizeof(test_chars) / sizeof(test_chars[0]);
+
+    for (size_t i = 0; i < num_tests; i++) {
+        char original = test_chars[i];
+        char std_result = toupper(original);
+        char ft_result = ft_toupper(original);
+
+        printf("Original: %c, Standard toupper: %c, ft_toupper: %c\n",
+               original, std_result, ft_result);
+    }
+
+    return 0;
+}
+*/
