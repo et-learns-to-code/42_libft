f6e1fbd061879fc88e5cf066346ab1314bfa1320
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2024-06-10 14:06:52 +0800

    all functions completed. testing will be carried out tomorrow

---
+++ ft_lstclear.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etien <etien@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 10:39:06 by etien             #+#    #+#             */
+/*   Updated: 2024/06/10 12:50:36 by etien            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+}
+
+/*
+#include <stdio.h>
+
+void del(void *content)
+{
+    free(content);
+}
+
+void print_list(t_list *lst)
+{
+    while (lst)
+    {
+        printf("%s -> ", (char *)lst->content);
+        lst = lst->next;
+    }
+    printf("NULL\n");
+}
+
+int main(void)
+{
+    t_list *list = ft_lstnew(strdup("First"));
+    list->next = ft_lstnew(strdup("Second"));
+    list->next->next = ft_lstnew(strdup("Third"));
+
+    printf("List before clearing:\n");
+    print_list(list);
+
+    ft_lstclear(&list, del);
+
+    printf("List after clearing:\n");
+    print_list(list);
+
+    return 0;
+}
+*/
